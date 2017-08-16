3ea97632689bdb2da71c3d9ad28d6233f9f07fe0
SalVN <sal.v.newell@gmail.com>
2017-08-16 16:16:59 +0100

    add form validation to front end

--- src/components/AddCamera.js
+++ src/components/AddCamera.js
@@ -2,23 +2,65 @@ import React, { Component } from 'react';
 import './AddCamera.css';
 import { connect } from 'react-redux';
 import * as actions from '../actions/actions';
+import {every} from 'underscore';
 import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom';
 
+function validate(state) {
+    const errors = {};
+    // test make
+    errors.make = (state.make.touched && state.make.value.length < 1) ? 'Please enter a camera make' : '';
+    // test model
+    errors.model = (state.model.touched && state.model.value.length < 1) ? 'Please enter a camera make' : '';
+    // test megapixels
+    if (state.megapixels.touched && state.megapixels.value.length < 1) errors.megapixels = 'Please enter the number of megapixels';
+    else if (state.megapixels.value.length > 0 && (/\D/g).test(state.megapixels.value)) errors.megapixels = 'Please enter only numbers';
+    else errors.megapixels = '';
+
+
+    return errors;
+}
 
 class AddCamera extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            added: false
+            added: false,
+            make: {
+                value: '',
+                touched: false
+            },
+            model: {
+                value: '',
+                touched: false
+            },
+            megapixels: {
+                value: '',
+                touched: false
+            },
+            errors: {
+                make: '',
+                model: '',
+                megapixels: ''
+            },
+            invalidEntries: false
         };
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeMake = this.handleChangeMake.bind(this);
+        this.handleChangeModel = this.handleChangeModel.bind(this);
+        this.handleChangeMegapixels = this.handleChangeMegapixels.bind(this);
     }
-    componentWillReceiveProps () {
-        if (this.props.added) this.setState({added: true});
+    componentWillReceiveProps() {
+        if (this.props.added) this.setState({ added: true });
     }
     render() {
+        let makeValidation = this.state.errors.make ? 'input-errors' : '';
+        let modelValidation = this.state.errors.model ? 'input-errors' : '';
+        let megapixelsValidation = this.state.errors.megapixels ? 'input-errors' : '';
         return (
             <div>
+                {this.state.invalidEntries &&
+                    <h3 className="validation">Unable to submit form</h3>
+                }
                 {
                     this.props.added &&
                     <Redirect to='/cameras' />
@@ -26,13 +68,19 @@ class AddCamera extends Component {
                 <h2>Add A Camera</h2>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="make">Make: </label>
-                    <input id="make" />
+                    <br />
+                    <input onChange={this.handleChangeMake} className={`cameraAddField ${makeValidation}`} id="make" placeholder="Camera Make" />
+                    <p className='validation'>{this.state.errors.make}</p>
                     <br />
                     <label htmlFor="cameraModel">Model: </label>
-                    <input id="cameraModel" />
+                    <br />
+                    <input onChange={this.handleChangeModel} className={`cameraAddField ${modelValidation}`} id="cameraModel" placeholder="Camera Model" />
+                    <p className='validation'>{this.state.errors.model}</p>
                     <br />
                     <label htmlFor="megapixels">Megapixels: </label>
-                    <input id="megapixels" />
+                    <br />
+                    <input onChange={this.handleChangeMegapixels} className={`cameraAddField ${megapixelsValidation}`} id="megapixels" placeholder="Megapixels" />
+                    <p className='validation'>{this.state.errors.megapixels}</p>
                     <br />
                     <button className="cameraAddButton" type="submit">Submit</button>
                     <Link to="/cameras"><button className="cameraReturnButton" type="submit">Return to Cameras</button></Link>
@@ -43,12 +91,45 @@ class AddCamera extends Component {
     }
     handleSubmit(e) {
         e.preventDefault();
-        const newCamera = {};
-        newCamera.make = e.target[0].value;
-        newCamera.model = e.target[1].value;
-        newCamera.megapixels = e.target[2].value;
-        console.log(newCamera);
-        this.props.addCamera(newCamera);
+        if (every(this.state.errors, field => field.length === 0)) {
+            const newCamera = {};
+            newCamera.make = e.target[0].value;
+            newCamera.model = e.target[1].value;
+            newCamera.megapixels = e.target[2].value;
+            this.props.addCamera(newCamera);
+            
+        } else {
+            this.setState({invalidEntries: true});
+        }
+
+    }
+    handleChangeMake(e) {
+        e.preventDefault();
+        const newState = Object.assign({}, this.state);
+        newState.make.value = e.target.value;
+        newState.make.touched = true;
+        const errors = validate(newState);
+        this.setState(Object.assign(newState, { errors }));
+    }
+    handleChangeModel(e) {
+        e.preventDefault();
+        const newState = Object.assign({}, this.state);
+        newState.model.value = e.target.value;
+        newState.model.touched = true;
+        newState.make.touched = true;
+        const errors = validate(newState);
+        this.setState(Object.assign(newState, { errors }));
+    }
+
+    handleChangeMegapixels(e) {
+        e.preventDefault();
+        const newState = Object.assign({}, this.state);
+        newState.megapixels.value = e.target.value;
+        newState.megapixels.touched = true;
+        newState.make.touched = true;
+        newState.model.touched = true;
+        const errors = validate(newState);
+        this.setState(Object.assign(newState, { errors }));
     }
 }
 
