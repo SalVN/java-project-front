68185a803ba9f1760f085cdbb9fa41cdeb6d5c47
SalVN <sal.v.newell@gmail.com>
2017-08-16 11:22:33 +0100

    get camera data using redux

--- src/components/CameraList.js
+++ src/components/CameraList.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/actions';
 import CameraBox from './CameraBox';
 
-const cameras = [
-    { cameraId: 1, megapixels: 24, make: 'Sony', model: 'A9' },
-    { cameraId: 2, megapixels: 16, make: 'Nikon', model: 'Coolpix P900' },
-    { cameraId: 3, megapixels: 30, make: 'Canon', model: 'EO5 5D Mark IV' },
-    { cameraId: 4, megapixels: 18, make: 'Panasonic', model: 'Lumix DMC-TZ60 ' },
-    { cameraId: 5, megapixels: 36, make: 'Nikon', model: 'D810' }
-];
+// const cameras = [
+//     { cameraId: 1, megapixels: 24, make: 'Sony', model: 'A9' },
+//     { cameraId: 2, megapixels: 16, make: 'Nikon', model: 'Coolpix P900' },
+//     { cameraId: 3, megapixels: 30, make: 'Canon', model: 'EO5 5D Mark IV' },
+//     { cameraId: 4, megapixels: 18, make: 'Panasonic', model: 'Lumix DMC-TZ60 ' },
+//     { cameraId: 5, megapixels: 36, make: 'Nikon', model: 'D810' }
+// ];
 
 class CameraList extends Component {
+    componentDidMount () {
+        this.props.fetchCameras();
+    }
     render() {
         return (
             <div>
-                {cameras.map((camera, i) => {
+                {this.props.cameras.map((camera, i) => {
                     return (
                         <CameraBox
                             key={i}
@@ -27,4 +32,18 @@ class CameraList extends Component {
     }
 }
 
-export default CameraList;
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchCameras: () => {
+      dispatch(actions.fetchCameras());
+    }
+  };
+}
+
+function mapStateToProps (state) {
+  return {
+    cameras: state.data,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CameraList);
