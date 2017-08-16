665a22ec6260bb572b14181ce602a02f105db48d
SalVN <sal.v.newell@gmail.com>
2017-08-16 10:00:10 +0100

    add a page header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import CameraList from './CameraList';
+import Header from './Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <CameraList />
       </div>
     );
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="AppHeader">
+                <h1 className="AppTitle">Cameras</h1>
+        </div>
+    );
+};
+
+export default Header;
