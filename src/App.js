d1334da3678883b89426a8acab5643b2bf509327
SalVN <sal.v.newell@gmail.com>
2017-08-16 10:02:44 +0100

    set up react router in App file

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import CameraList from './CameraList';
 import Header from './Header';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <CameraList />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path='/' component={CameraList} />
+            <Route exact path='/cameras' component={CameraList} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
