7912b34a7dfd778fdef7c47d2d761f5cf6275e86
SalVN <sal.v.newell@gmail.com>
2017-08-16 10:08:49 +0100

    renders cameras for route /cameras using hard-coded data

--- src/CameraBox.js
+++ src/CameraBox.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import './CameraBox.css';
+import PropTypes from 'prop-types';
 
 class CameraBox extends Component {
     render () {
         let colour = (this.props.index % 2 === 0) ? 'even' : 'odd';
         return (
             <div className={`cameraBox ${colour}`}>
-                <h3 className="cameraBoxTitle">Make</h3>
-                <h4 className="cameraBoxSubtitle">Model</h4>
+                <h3 className="cameraBoxTitle">{this.props.camera.make}</h3>
+                <h4 className="cameraBoxSubtitle">{this.props.camera.model}</h4>
                 <button className="cameraBoxButton" disabled>More Info</button>
             </div>
         );
     }
 }
+
+CameraBox.propTypes = {
+    camera: PropTypes.object.isRequired
+};
 
 export default CameraBox;
--- src/CameraList.js
+++ src/CameraList.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import CameraBox from './CameraBox';
 
+const cameras = [
+    { cameraId: 1, megapixels: 24, make: 'Sony', model: 'A9' },
+    { cameraId: 2, megapixels: 16, make: 'Nikon', model: 'Coolpix P900' },
+    { cameraId: 3, megapixels: 30, make: 'Canon', model: 'EO5 5D Mark IV' },
+    { cameraId: 4, megapixels: 18, make: 'Panasonic', model: 'Lumix DMC-TZ60 ' },
+    { cameraId: 5, megapixels: 36, make: 'Nikon', model: 'D810' }
+];
+
 class CameraList extends Component {
     render() {
         return (
             <div>
-                <CameraBox />
+                {cameras.map((camera, i) => {
+                    return (
+                        <CameraBox
+                            key={i}
+                            index={i}
+                            camera={camera} />
+                    );
+                })
+                }
             </div>
         );
     }
