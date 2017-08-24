2ef9660c155a23190141e7704b2c5072f2f1784b
SalVN <sal.v.newell@gmail.com>
2017-08-24 15:26:06 +0100

    update config file

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    API_URL: 'http://ec2-54-77-175-175.eu-west-1.compute.amazonaws.com:6061/'
+    API_URL: function (env) {
+        if (env === 'prod') return 'http://ec2-54-77-175-175.eu-west-1.compute.amazonaws.com:6061/'
+        else return 'http://localhost:9092'
+    }
 }
